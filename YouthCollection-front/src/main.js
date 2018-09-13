71c82f589fd1afb6d4bbb539dd10e23a99cfb28a
SpringLych <2571568860@qq.com>
2018-09-13 12:45:25 +0800

    部分优化

--- YouthCollection-front/src/main.js
+++ YouthCollection-front/src/main.js
@@ -5,6 +5,9 @@ import App from './App';
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+// 添加 Fastclick 移除移动端点击延迟
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
